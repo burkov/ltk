05930060f3e2e9b1fd1a29cf19a168fb33d2c974
alexander.burkov <alexander.burkov@jetbrains.com>
2018-12-12 01:03:01 +0300

    apt-upgrade-service: stop default apt-daily service

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ cp systemd/apt-upgrade/apt-upgrade-service /usr/local/bin
 
 systemctl enable apt-upgrade.timer
 systemctl start apt-upgrade.timer
+systemctl disable apt-daily-upgrade.timer
+systemctl stop apt-daily-upgrade.timer
+systemctl disable apt-daily.timer
+systemctl stop apt-daily.timer
