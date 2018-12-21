f2fe6636a240a7036ceba43196f1fc7246f1e454
alexander.burkov <alexander.burkov@jetbrains.com>
2018-12-21 23:43:52 +0300

    Add off-on service

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp systemd/apt-upgrade/apt-upgrade.service /etc/systemd/system
 cp systemd/apt-upgrade/apt-upgrade.timer   /etc/systemd/system
 cp systemd/apt-upgrade/apt-upgrade-service /usr/local/bin
 
-chmod +x /usr/local/bin
+chmod +x /usr/local/bin/apt-upgrade-service
 
 systemctl enable apt-upgrade.timer
 systemctl start apt-upgrade.timer
@@ -15,3 +15,16 @@ systemctl disable apt-daily-upgrade.timer
 systemctl stop apt-daily-upgrade.timer
 systemctl disable apt-daily.timer
 systemctl stop apt-daily.timer
+
+################
+
+echo ":: Installing off-on service"
+
+cp systemd/off-on/off-on.service /etc/systemd/system
+cp systemd/off-on/off-on.timer   /etc/systemd/system
+cp systemd/off-on/off-on-service /usr/local/bin
+
+chmod +x /usr/local/bin/off-on-service
+
+systemctl enable off-on.timer
+systemctl start off-on.timer
